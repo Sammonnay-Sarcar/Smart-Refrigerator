0045c9398460ec3cacd747a8897f2598e75b5ff5
Sammonnay-Sarcar <77871122+Sammonnay-Sarcar@users.noreply.github.com>
2023-03-23 21:39:45 +0530

    skeleton made

--- lib/bottomNavigationBar.dart
+++ lib/bottomNavigationBar.dart
@@ -1,3 +1,4 @@
+import 'package:cold_storage/common/widgets/app_bar.dart';
 import 'package:cold_storage/features/Content/screen/content_screen.dart';
 import 'package:cold_storage/features/Home%20Screen/screen/Home_Screen.dart';
 import 'package:cold_storage/features/Location/screen/location_screen.dart';
@@ -32,6 +33,37 @@ class _RoundedBottomNavigationBarState
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: NeumorphicAppBar(
+        title: NeumorphicText(
+          'Smart\nFreezer',
+          textAlign: TextAlign.left,
+          textStyle: NeumorphicTextStyle(
+            fontSize: 25,
+            fontFamily: 'Poppins',
+            height: 1,
+          ),
+          style: const NeumorphicStyle(
+            depth: 5,
+            color: Colors.black,
+            shadowDarkColor: Colors.grey,
+          ),
+        ),
+        actions: <Widget>[
+          NeumorphicButton(
+              style: const NeumorphicStyle(
+                intensity: 5,
+                depth: 3,
+                lightSource: LightSource.topLeft,
+                shape: NeumorphicShape.flat,
+                color: Color(0xE8DEDEDE),
+              ),
+              onPressed: () {},
+              child: const Icon(
+                Icons.account_box_rounded,
+                size: 45,
+              ))
+        ],
+      ),
       body: _pages[_selectedIndex],
       bottomNavigationBar: Container(
         height: 100,
