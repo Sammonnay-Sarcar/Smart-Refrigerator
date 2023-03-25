2e37bb947048de126310cb8eef7f2578df6247a5
Sammonnay-Sarcar <77871122+Sammonnay-Sarcar@users.noreply.github.com>
2023-03-25 10:05:42 +0530

    temp

--- lib/features/Home Screen/screen/home_screen.dart
+++ lib/features/Home Screen/screen/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:knob_widget/knob_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,10 +9,55 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final double _minValue = 0;
+  final double _maxValue = 100;
+  final double _optimalValue = 50;
+
+  late KnobController _knobController;
+  late double _knobValue;
+  void valueChangedListener(double value) {
+    if (mounted) {
+      setState(() {
+        _knobValue = value;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _knobValue = _minValue;
+    _knobController = KnobController(
+        initial: _knobValue,
+        minimum: _minValue,
+        maximum: _maxValue,
+        startAngle: -50,
+        endAngle: 230,
+        precision: 2);
+    _knobController.addOnValueChangedListener(valueChangedListener);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Home Screen"),
-    );
+    return Center(
+        child: Container(
+      child: Knob(
+        controller: _knobController,
+        width: 200,
+        height: 200,
+        style: KnobStyle(
+            labelStyle: Theme.of(context).textTheme.bodyLarge,
+            tickOffset: 5,
+            labelOffset: 10,
+            minorTicksPerInterval: 10,
+            showMinorTickLabels: true),
+      ),
+    ));
+  }
+
+  @override
+  void dispose() {
+    _knobController.removeOnValueChangedListener(valueChangedListener);
+    super.dispose();
   }
 }
